a2026ea98366f4427578e059fef4d4ff2782cd67
Oleksandr Klepinin <aleksandrklepinin29@gmail.com>
2024-02-16 13:58:13 +0200

    HW-34

--- HW-34/todo-list/src/core/init/store.js
+++ HW-34/todo-list/src/core/init/store.js
@@ -1,17 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootSaga from './rootSaga';
 import todosReducer from '../data/todosSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const sagaMiddleware = createSagaMiddleware();
 
+const rootReducer = combineReducers({
+    todos: todosReducer,
+});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['todos']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-    reducer: {
-        todos: todosReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/REGISTER'],
+            },
+        }).concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 
 export default store;
--- HW-34/todo-list/src/main.jsx
+++ HW-34/todo-list/src/main.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './core/init/store';
-import App from './ui/containers/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import store, { persistor } from './core/init/store';
+import App from './ui/containers/App.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
+            <PersistGate loading={null} persistor={persistor}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </PersistGate>
         </Provider>
     </React.StrictMode>,
 );
